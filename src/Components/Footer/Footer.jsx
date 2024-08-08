f3292f4d5ff5471f7c78120e16b6843f5835030e
mdshafin90 <mdnazmussakibshafin@gmail.com>
2024-08-08 19:53:33 +0600

    react toastify added and footer part updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,24 +1,48 @@
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+import { styled } from '@mui/material/styles';
+import * as React from 'react';
 import { FaFacebook, FaInstagram, FaInternetExplorer, FaLinkedin } from 'react-icons/fa';
 import { FaSquareXTwitter } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import logo from '../../../public/restaurant-icon.png';
 
+const BootstrapTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} arrow classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.arrow}`]: {
+    color: theme.palette.common.black,
+  },
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.common.black,
+  },
+}));
+
 const Footer = () => {
+
+  const handleClick = () => {
+    toast.warn('this is just for portfolio!', {
+        theme: "dark",
+        autoClose: 2000,
+    })
+}
+
   return (
     <>
       <footer className="footer mt-10 bg-gradient-to-r from-blue-950 via-purple-950 to-blue-950 text-white text-base md:text-xl p-10">
         <nav>
           <h6 className="footer-title">Menu</h6>
-          <a className="link link-hover">Appetizers</a>
-          <a className="link link-hover">Main Courses</a>
-          <a className="link link-hover">Desserts</a>
-          <a className="link link-hover">Beverages</a>
+          <a onClick={handleClick} className="link link-hover">Appetizers</a>
+          <a onClick={handleClick} className="link link-hover">Main Courses</a>
+          <a onClick={handleClick} className="link link-hover">Desserts</a>
+          <a onClick={handleClick} className="link link-hover">Beverages</a>
         </nav>
         <nav>
           <h6 className="footer-title">Contact Us</h6>
           <a className="link link-hover">Phone: (123) 456-7890</a>
-          <a className="link link-hover">Email: contact@restaurant.com</a>
-          <a className="link link-hover">Location: Dhaka, Bangladesh</a>
+          <a onClick={handleClick} className="link link-hover">Email: contact@restaurant.com</a>
+          <a onClick={handleClick} className="link link-hover">Location: Dhaka, Bangladesh</a>
         </nav>
         <nav>
           <h6 className="footer-title">Opening Hours</h6>
@@ -38,23 +62,34 @@ const Footer = () => {
         </aside>
         <nav className="md:place-self-center md:justify-self-end">
         <div className="grid grid-flow-col gap-4">
+      <BootstrapTooltip title="Facebook">
       <Link to="https://www.facebook.com/ns.shafin.5/" target='_blank'>
         <FaFacebook size={24} />
       </Link>
+      </BootstrapTooltip>
+      <BootstrapTooltip title="Instagram">
       <Link to="https://www.instagram.com/mdnazmussakibshafi/" target='_blank'>
         <FaInstagram size={24} />
       </Link>
+      </BootstrapTooltip>
+      <BootstrapTooltip title="Twitter / X">
       <Link to="https://x.com/nazmus_sakib90" target='_blank'>
         <FaSquareXTwitter size={24} />
       </Link>
+      </BootstrapTooltip>
+      <BootstrapTooltip title="LinkedIn">
       <Link to="https://www.linkedin.com/in/md-nazmus-sakib-shafin-781357254/" target='_blank'>
         <FaLinkedin size={24} />
       </Link>
+      </BootstrapTooltip>
+      <BootstrapTooltip title="Website">
       <Link to="https://shafin-portfolio-site.netlify.app/" target='_blank'>
         <FaInternetExplorer size={24} />
       </Link>
+      </BootstrapTooltip>
     </div>
         </nav>
+        <ToastContainer />
       </footer>
     </>
   );
